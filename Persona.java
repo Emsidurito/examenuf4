69daffa3641e2fa454729955368bbcd0850d2b8c
Alejandro <aleejandronapal@gmail.com>
2022-06-01 08:48:36 +0200

    Versio 2

--- Persona.java
+++ Persona.java
@@ -1,34 +1,34 @@
-public class Persona {
-    private int num_dni;
+public class Persona { //Creem la class Persona que contindra diferents atributs
+    private int num_dni; //Initzalitzem els valors de manera global
     private String nom;
     private String adreça;
 
     Persona(int pdni) {
-        num_dni = pdni;
+        num_dni = pdni;  //dona valor a num_dni amb la variable entrada a pdni
     }
 
-    Persona(int pdni, String pnom) {
-        num_dni = pdni;
-        nom = pnom;
+    Persona(int pdni, String pnom) {  //Variables que recupera Persona
+        num_dni = pdni; 
+        nom = pnom;  //dona valor a nom amb la variable entrada a pnom
     }
 
     public int getDNI() {
-        return (num_dni);
+        return (num_dni);  //quan es requereixi el DNI aquest int ens retornara el num_dni
     }
 
     public String getNom() {
-        return (nom);
+        return (nom);   //quan es requereixi el Nom getNom ens retornara el nom
     }
 
     public setNom(String pnom) {
-        nom = pnom;
+        nom = pnom; //al utilitzar setNom podrem alterar el valor de nom 
     }
 
     public String getAdreça() {
-        return (adreça);
+        return (adreça);  //quan es requereixi la adreça aquest int ens retornara l'adreça
     }
 
     public setAdreça(String padreça) {
-        adreça = padreça;
+        adreça = padreça;  //al utilitzar setadreça podrem alterar el valor de adreça  
     }
 }
